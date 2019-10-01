df44278e613fc2da379afa714f019a4db86e53a1
Zbigniew Skolicki <skolicki@users.noreply.github.com>
2019-10-01 18:24:41 +0200

    shorten and simplify

--- snake.py
+++ snake.py
@@ -1,30 +1,16 @@
+# A simple recursive code to solve putting together https://en.wikipedia.org/wiki/Snake_cube
+# Reformatted a bit aiming at brevity :)
+# Hint: if you want to actually follow the result, it's physically easier going backwards
+
 import copy
 
-legs = [2, 2, 2, 2, 1, 1, 1, 2, 2, 1, 1, 2, 1, 2, 1, 1, 2]
-
-def add(v1, v2):
-    return [v1[i] + v2[i] for i in range(3)]
-
-def mult(v, a):
-    return [v[i] * a for i in range(3)]
-
-def in_bounds(pos):
-    return all(pos[i] in (0, 1, 2) for i in range(3))
-
-def free(cube, pos):
-    return not cube[pos[0]][pos[1]][pos[2]]
-
-def fill(cube, pos):
-    cube[pos[0]][pos[1]][pos[2]] = 1
-
-def move(cube, pos, leg, moves, dir):
-    for d in range(legs[leg]):
-        pos2 = add(pos, mult(dir, d + 1))
-        if not in_bounds(pos2) or not free(cube, pos2):
+def move(cube, pos, legs, path, dir):
+    for d in range(legs[0]):
+        pos = [pos[i] + dir[i] for i in range(3)]
+        if not all(pos[i] in (0, 1, 2) for i in range(3)) or cube[pos[0]][pos[1]][pos[2]]:
             return False
-        fill(cube, pos2)
-    new_pos = add(pos, mult(dir, legs[leg]))
-    return check(cube, new_pos, leg + 1, moves + [new_pos])
+        cube[pos[0]][pos[1]][pos[2]] = 1
+    return check(cube, pos, legs[1:], path + [pos])
 
 def check(cube, pos, leg, moves):
     if leg == len(legs):
@@ -33,6 +19,8 @@ def check(cube, pos, leg, moves):
         return True
     return any(move(copy.deepcopy(cube), pos, leg, moves, dir) for dir in ([-1, 0, 0], [1, 0, 0], [0, -1, 0], [0, 1, 0], [0, 0, -1], [0, 0, 1]))
 
-cube = [[[None, None, None], [None, None, None], [None, None, None]], [[None, None, None], [None, None, None], [None, None, None]], [[None, None, None], [None, None, None], [None, None, None]]]
-fill(cube, [0, 0, 0])
-check(cube, [0, 0, 0], 0, [])
+check(
+    [[[1, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0]]],  # already fill at [0, 0, 0]
+    [0, 0, 0],  # 4 legs of length 2 implies starting in a corner
+    [2, 2, 2, 2, 1, 1, 1, 2, 2, 1, 1, 2, 1, 2, 1, 1, 2],  # hardcoded physical toy
+    [])
