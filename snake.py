c32dbe3089c902f1656c75c59a34d4bf53a74899
Zbigniew Skolicki <skolicki@users.noreply.github.com>
2019-10-06 00:03:17 +0200

    shorten dir name

--- snake.py
+++ snake.py
@@ -4,16 +4,16 @@
 
 import copy
 
-def move(cube, pos,  dir_index, sgn, legs, path):
-    for d in range(legs[0]):
-        pos[dir_index] += sgn
-        if not pos[dir_index] in range(3) or cube[pos[0]][pos[1]][pos[2]]:
-            return False
-        cube[pos[0]][pos[1]][pos[2]] = 1
-    if not legs[1:]:
+def move(cube, pos, dir, sgn, legs, path):
+    if not legs:
         print(path)
         return True
-    return any(move(copy.deepcopy(cube), copy.copy(pos), dir_index, sgn, legs[1:], path + [pos]) for dir_index in range(3) for sgn in (-1, 1))
+    for d in range(legs[0]):
+        pos[dir] += sgn
+        if not pos[dir] in range(3) or cube[pos[0]][pos[1]][pos[2]]:
+            return False
+        cube[pos[0]][pos[1]][pos[2]] = 1
+    return any(move(copy.deepcopy(cube), copy.copy(pos), dir, sgn, legs[1:], path + [pos]) for dir in range(3) for sgn in (-1, 1))
 
 move(
     [[[1, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0]]],  # already fill at [0, 0, 0]
